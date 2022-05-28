2f009cfee3f60bc8c99b80e4a339e27498a69bbe
Vinicius Luna <viniciuscluna@gmail.com>
2022-05-28 19:52:55 -0300

    Added Gateway

---
+++ src/main/java/luna/carmo/vinicius/letscodegateway/LetscodeGatewayApplication.java
@@ -0,0 +1,26 @@
+package luna.carmo.vinicius.letscodegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class LetscodeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetscodeGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/api/sale/**")
+                        .uri("lb://sale/"))
+                .build();
+    }
+}
---
+++ src/test/java/luna/carmo/vinicius/letscodegateway/LetscodeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package luna.carmo.vinicius.letscodegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetscodeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
